d49c6ac7d8b639f6ef6d4ad63adee071fcec9e4a
arafat.hossain <arafat.hossain@petronas.com>
2022-09-02 10:43:13 +0600

    init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(()=>{
+      window.$crisp = [];
+      window.CRISP_WEBSITE_ID = process.env.NEXT_PUBLIC_CRIST_WEBSITE_ID;
+      (() => {
+        const d = document;
+        const s:any = d.createElement("script");
+        s.src = "https://client.crisp.chat/l.js";
+        s.async = 1;
+        d.getElementsByTagName("body")[0].appendChild(s);
+      })();
+      
+  })
+
   return <Component {...pageProps} />
 }
 
